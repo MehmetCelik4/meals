1b4e4bbccadd5aacc674cd7b262d8c4cfa9fa05f
MehmetCelik4 <mehmetcelik4@gmail.com>
2020-04-29 12:59:17 +0300

    add category screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+import 'package:meals/category_item.dart';
+import 'package:meals/dummy_data.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: Center(
+        child: GridView(
+          children: DUMMY_CATEGORIES
+              .map(
+                (category) => CategoryItem(
+                  category.title,
+                  category.color,
+                ),
+              )
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ),
       ),
     );
   }
