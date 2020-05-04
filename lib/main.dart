c8dae1da4f353bbb553b1bf939a6b69a515cef65
MehmetCelik4 <mehmetcelik4@gmail.com>
2020-05-04 19:27:05 +0300

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/filters_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/screens/tabs_screen.dart';
 import 'screens/categories_screen.dart';
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
     );
   }
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 import 'favorites_screen.dart';
 
@@ -29,6 +30,9 @@ class _TabsScreenState extends State<TabsScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text(_pages[_selectedPageIndex]["title"]),
+        ),
+        drawer: Drawer(
+          child: MainDrawer(),
         ),
         body: _pages[_selectedPageIndex]["page"],
         bottomNavigationBar: BottomNavigationBar(
